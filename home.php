8fa695eeda177f663bf7cf576552ee9f7a0a1be2
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-07-05 19:34:43 -0600

    set navbar links

--- home.php
+++ home.php
@@ -3,7 +3,9 @@
 <body>
 
 	<!-- navbar -->
-	<?php require_once ("partials/navbar.php");?>
+	<div class="container-fluid">
+		<?php require_once ("partials/navbar.php");?>
+	</div>
 
 	<!-- bootstrap scripts -->
 	<?php require_once ("partials/scripts.php")?>
